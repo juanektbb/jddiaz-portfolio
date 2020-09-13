3c7e4ad854a4e0319715a6d0dafe08697b353db9
Juan D. Diaz <inf.juandiaz@gmail.com>
2020-09-13 19:08:01 +0300

    Styles updated for all views

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -69,11 +69,6 @@ class Work extends React.Component{
           </div>
 
           <div className="double-column">
-            <div className="download-cv">
-              <div>Download CV</div>
-              <img src={Download} alt='Download icon'/>
-            </div>
-
             <h2>Freelancer or Contractor</h2>
             <h4>Shall we work on your next project?</h4>
             <h5 className="last-heading">Let's chat and share ideas!</h5>
@@ -94,6 +89,11 @@ class Work extends React.Component{
             <p>
               If this sounds interesting to you, <Link to="/chat">get in touch</Link> to discuss your project in more detail, share ideas and get an amazing outcome from each others collaboration.
             </p>
+
+            <div className="download-cv">
+              <div>Download CV</div>
+              <img src={Download} alt='Download icon'/>
+            </div>
           </div>
         </div>
       </div>
